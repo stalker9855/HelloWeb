e0f8f319f163e7b01ff8873ea74ca86bebe2cf9f
stalker9855 <andrey.l.evseev@gmail.com>
2023-10-22 00:11:17 +0300

    .

---
+++ FileDownload/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace FileDownload.Controllers
+{
+    public class FileController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public FileResult GenerateFile(string firstName, string lastName, string fileName)
+        {
+            string content = $"First Name: {firstName}{Environment.NewLine}Last name: {lastName}";
+
+            var byteArray = Encoding.UTF8.GetBytes(content);
+            var stream = new MemoryStream(byteArray);
+
+            return File(stream, "text/plain", $"{fileName}.txt");
+        }
+    }
+}
