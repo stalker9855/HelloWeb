9a5bba3b4eee48a6aa6cb51a056467bd5fa4b7de
stalker9855 <andrey.l.evseev@gmail.com>
2023-11-12 09:02:14 +0200

    .

---
+++ lr10/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using lr10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lr10.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public bool isValidDate(ConsultationModel consultationModel)
+        {
+            if(consultationModel.Date <= DateTime.Now)
+            {
+                ModelState.AddModelError("Date", "The date must be in the future");
+                return false;
+            }
+            if(consultationModel.Date.DayOfWeek == DayOfWeek.Saturday 
+                || consultationModel.Date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                ModelState.AddModelError("Date", "Consultations are not available on weekends");
+                return false;
+            }
+            if(consultationModel.SelectedLanguage == "Basis" && consultationModel.Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                ModelState.AddModelError("Date", "Consultations 'Basis' are not available on Mondays");
+                return false;
+            }
+            return true;
+        }
+        
+        public IActionResult Index(ConsultationModel consultationModel)
+        {
+            if(isValidDate(consultationModel))
+            {
+                return RedirectToAction("SuccessForm", consultationModel);
+            }
+            return View();
+        }
+        public IActionResult SuccessForm(ConsultationModel consultationModel)
+        {
+            return View(consultationModel);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ lr10/Models/ConsultationModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lr10.Models
+{
+    public class ConsultationModel
+    {
+        [Required (ErrorMessage = "Name not specifed")]
+        public string Name { get; set; }
+        [Required (ErrorMessage = "Email not specifed")]
+        public string Email { get; set; }
+        [Required (ErrorMessage = "Date not specifed")]
+        public DateTime Date { get; set; }
+        [Required(ErrorMessage = "Language not specifed")]
+        public string SelectedLanguage { get; set; } = "Basis";
+       
+    }
+}
